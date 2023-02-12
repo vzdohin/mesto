a2368e30bfc396db177aeef5e71b95ed4810ed24
vzdohin <peenky88@yandex.ru>
2023-02-12 23:20:03 +0500

    подправил JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
-let editProfileButton = document.querySelector('.profile__edit-button');
 let editPopup = document.querySelector('.popup');
+let editProfileButton = document.querySelector('.profile__edit-button');
 let editPopupCloseButton = document.querySelector('.popup__close-button');
+let popupSaveButton = document.querySelector('.popup__save-button');
 
+let userNameElement = document.querySelector('.profile__name');
+let userNameInput = document.querySelector('.popup__input_data_name');
+let userAboutElement = document.querySelector('.profile__about');
+let userAboutInput = document.querySelector('.popup__input_data_about');
 
 editProfileButton.addEventListener('click', function () {
   openPopup(editPopup);
+  userNameInput.value = userNameElement.textContent;
+  userAboutInput.value = userAboutElement.textContent;
 })
 
 editPopupCloseButton.addEventListener('click', function () {
@@ -17,20 +24,6 @@ function openPopup(popup) {
 function closePopup(popup) {
   popup.classList.remove('popup_opened')
 }
-
-let popupSaveButton = document.querySelector('.popup__save-button');
-
-let userNameElement = document.querySelector('.profile__name');
-let userNameInput = document.querySelector('.popup__input_data_name');
-editProfileButton.addEventListener('click', function () {
-  userNameInput.value = userNameElement.textContent;
-})
-
-let userAboutElement = document.querySelector('.profile__about');
-let userAboutInput = document.querySelector('.popup__input_data_about');
-editProfileButton.addEventListener('click', function () {
-  userAboutInput.value = userAboutElement.textContent;
-})
 
 let formElement = document.querySelector('.popup__form');
 
