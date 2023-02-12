eaffe4879c08c91537c5103f532eda1dcf664f79
vzdohin <peenky88@yandex.ru>
2023-02-12 22:56:22 +0500

    исправлены ошибки после первой ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,43 @@ let editProfileButton = document.querySelector('.profile__edit-button');
 let editPopup = document.querySelector('.popup');
 let editPopupCloseButton = document.querySelector('.popup__close-button');
 
-editProfileButton.addEventListener('click', function() {
-  editPopup.classList.add('popup_opened');
+
+editProfileButton.addEventListener('click', function () {
+  openPopup(editPopup);
 })
 
-editPopupCloseButton.addEventListener('click', function() {
-  editPopup.classList.remove('popup_opened');
+editPopupCloseButton.addEventListener('click', function () {
+  closePopup(editPopup);
 })
 
 function openPopup(popup) {
-  popup.classList.add('.popup_opened')
+  popup.classList.add('popup_opened')
 }
-
 function closePopup(popup) {
-  popup.classList.remove('.popup_opened')
+  popup.classList.remove('popup_opened')
 }
 
-let userNameElement = document.querySelector('#userName');
-let userNameInput = document.querySelector('#userNameInput');
-userNameInput.value = userNameElement.textContent;
+let popupSaveButton = document.querySelector('.popup__save-button');
 
-let userAboutElement = document.querySelector('#userAbout');
-let userAboutInput = document.querySelector('#userAboutInput');
-userAboutInput.value = userAboutElement.textContent;
-
-
-let formElement = document.querySelector('.popup__form'); 
-
-function handleFormSubmit (evt) {
-    evt.preventDefault();     
-    userNameElement.textContent = userNameInput.value;
-    userAboutElement.textContent = userAboutInput.value;
-}
-formElement.addEventListener('submit', handleFormSubmit); 
-
-let submitButton = document.querySelector('.popup__save-button');
-submitButton.addEventListener('click', function() {
-  editPopup.classList.remove('popup_opened');
+let userNameElement = document.querySelector('.profile__name');
+let userNameInput = document.querySelector('.popup__input_data_name');
+editProfileButton.addEventListener('click', function () {
+  userNameInput.value = userNameElement.textContent;
 })
+
+let userAboutElement = document.querySelector('.profile__about');
+let userAboutInput = document.querySelector('.popup__input_data_about');
+editProfileButton.addEventListener('click', function () {
+  userAboutInput.value = userAboutElement.textContent;
+})
+
+let formElement = document.querySelector('.popup__form');
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  userNameElement.textContent = userNameInput.value;
+  userAboutElement.textContent = userAboutInput.value;
+  editPopup.classList.remove('popup_opened')
+}
+formElement.addEventListener('submit', handleFormSubmit);
+
