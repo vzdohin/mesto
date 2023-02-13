18193acfc552d8371a451101cd844799b07e4085
vzdohin <peenky88@yandex.ru>
2023-02-13 15:56:58 +0500

    исправлены ошибки 2 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 let editPopup = document.querySelector('.popup');
 let editProfileButton = document.querySelector('.profile__edit-button');
 let editPopupCloseButton = document.querySelector('.popup__close-button');
-let popupSaveButton = document.querySelector('.popup__save-button');
 
 let userNameElement = document.querySelector('.profile__name');
 let userNameInput = document.querySelector('.popup__input_data_name');
@@ -31,7 +30,7 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
   userNameElement.textContent = userNameInput.value;
   userAboutElement.textContent = userAboutInput.value;
-  editPopup.classList.remove('popup_opened')
+  closePopup(editPopup);
 }
 formElement.addEventListener('submit', handleFormSubmit);
 
