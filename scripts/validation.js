237dc925614bd58e672807c051299da2ec497131
vzdohin <peenky88@yandex.ru>
2023-03-12 22:26:54 +0500

    исправил ошибки автотеста

--- scripts/validation.js
+++ scripts/validation.js
@@ -3,7 +3,7 @@ const configValidation = {
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save-button',
   inactiveButtonClass: 'popup__save-button_disabled',
-  inputErrorClass: 'popup__input_type_error',
+  inputErrorClass: 'popup__input_error-style',
 }
 
 const showInputError = (errorText, validationMessage, inputErrorClass) => {
